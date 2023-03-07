017065703db5d9ac5f97282438f703b4d0538247
Jean Porto <portojean@gmail.com>
2023-03-07 07:17:11 -0300

    feat: adiciona metodo create

--- contact_list_beta2/app/controllers/sessions_controller.rb
+++ contact_list_beta2/app/controllers/sessions_controller.rb
@@ -3,6 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session]{:password})
+      sign_in(user)
+    end
   end
 
 end
